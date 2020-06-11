ebdaf6d2eb3a06d2eef6193d094ed22de3e1af83
Vlad Emelianov <volshebnyi@gmail.com>
2020-06-11 15:00:28 +0200

    Add sanity check script

---
+++ scripts/check_output.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT_PATH=$(dirname $(dirname $(realpath $0)))
+cd ${ROOT_PATH}
+START="$1"
+
+PACKAGES=`find mypy_boto3_output -name '__init__.py' | sort`
+
+for f in $PACKAGES; do
+    DIRNAME="$(dirname "$f")"
+    if [[ ! "$f" == *"$START"* ]]; then
+        continue
+    fi
+    echo "Checking $DIRNAME"
+    pylint $DIRNAME || true
+    mypy $DIRNAME || true
+done
