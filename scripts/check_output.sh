0e461949e3089e11ea03115b1113b815b3569e8e
Vlad Emelianov <volshebnyi@gmail.com>
2020-08-31 22:36:31 +0200

    Add new services support

--- scripts/check_output.sh
+++ scripts/check_output.sh
@@ -5,7 +5,7 @@ ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 cd ${ROOT_PATH}
 START="$1"
 
-PACKAGES=`find mypy_boto3_output -name '__init__.py' | sort`
+PACKAGES=`find mypy_boto3_output -name 'type_defs.py' | sort`
 
 for f in $PACKAGES; do
     DIRNAME="$(dirname "$f")"
@@ -13,6 +13,6 @@ for f in $PACKAGES; do
         continue
     fi
     echo "Checking $DIRNAME"
-    pylint $DIRNAME
-    mypy $DIRNAME
+    pylint $DIRNAME || true
+    mypy $DIRNAME || true
 done
