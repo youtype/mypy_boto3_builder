8a2004cc4bbfab7f088ddbe826e4977a22f0380e
Vlad Emelianov <volshebnyi@gmail.com>
2020-10-28 19:21:29 +0300

    Update dependencies

--- scripts/check_output.sh
+++ scripts/check_output.sh
@@ -3,15 +3,11 @@ set -e
 
 ROOT_PATH=$(dirname $(dirname $0))
 cd ${ROOT_PATH}
-START="$1"
 
-PACKAGES=`find mypy_boto3_output -name 'type_defs.pyi' | sort`
+PACKAGES=`find mypy_boto3_output -name 'type_defs.pyi' | grep "${1}" | sort`
 
 for f in $PACKAGES; do
     DIRNAME="$(dirname "$f")"
-    if [[ ! "$f" == *"$START"* ]]; then
-        continue
-    fi
     echo "Checking $DIRNAME"
     pylint $DIRNAME
     mypy $DIRNAME
