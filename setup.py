7a52660c21a3da33961d96db681a5a5249e567c4
Vlad Emelianov <volshebnyi@gmail.com>
2021-04-14 21:04:27 +0300

    Update dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
-from pathlib import Path
-
-import setuptools
+from setuptools import setup
 
 if __name__ == "__main__":
-    setuptools.setup(
-        install_requires=[
-            line
-            for line in (Path(__file__).parent / "requirements.txt").read_text().split("\n")
-            if line.strip() and not line.startswith("-")
-        ]
-    )
+    setup()
