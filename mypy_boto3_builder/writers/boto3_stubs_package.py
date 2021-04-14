74f358d6e2049938fb76def63c60117fc6b80a19
Vlad Emelianov <volshebnyi@gmail.com>
2021-04-14 21:30:20 +0300

    Fix direct import of main package

--- mypy_boto3_builder/writers/boto3_stubs_package.py
+++ mypy_boto3_builder/writers/boto3_stubs_package.py
@@ -44,7 +44,7 @@ def write_boto3_stubs_package(
         [
             (package_path / "py.typed", module_templates_path / "py.typed.jinja2"),
             (package_path / "__init__.pyi", module_templates_path / "__init__.pyi.jinja2"),
-            (package_path / "__init__.py", module_templates_path / "__init__.pyi.jinja2"),
+            (package_path / "__init__.py", module_templates_path / "__init__.py.jinja2"),
             (package_path / "session.pyi", module_templates_path / "session.pyi.jinja2"),
             (package_path / "__main__.py", module_templates_path / "__main__.py.jinja2"),
             (package_path / "version.py", module_templates_path / "version.py.jinja2"),
