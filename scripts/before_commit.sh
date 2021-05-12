0c69a66f0206caa50473f852006389b35e92f40d
Vlad Emelianov <volshebnyi@gmail.com>
2021-05-12 16:55:03 +0300

    Remove pyright from pre-commit

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -6,7 +6,7 @@ cd $ROOT_PATH
 
 # vulture mypy_boto3_builder --make-whitelist > vulture_whitelist.txt
 vulture mypy_boto3_builder vulture_whitelist.txt
-npx pyright mypy_boto3_builder
+# npx pyright mypy_boto3_builder
 flake8 mypy_boto3_builder
 pytest
 black mypy_boto3_builder tests
