f00460416c442b84ff0cbcd11bf687630714a902
Vlad Emelianov <volshebnyi@gmail.com>
2021-07-02 17:19:36 +0300

    Update unit tests

--- tests/test_cli_parser.py
+++ tests/test_cli_parser.py
@@ -1,6 +1,6 @@
 from unittest.mock import MagicMock, patch
 
-from mypy_boto3_builder.cli_parser import get_absolute_path, get_service_name, parse_args
+from mypy_boto3_builder.cli_parser import get_absolute_path, parse_args
 
 
 class TestCLIParser:
@@ -14,13 +14,3 @@ class TestCLIParser:
         result = get_absolute_path("test/output")
         PathMock.assert_called_with("test/output")
         assert result == PathMock().absolute()
-
-    @patch("mypy_boto3_builder.cli_parser.ServiceNameCatalog")
-    def test_get_service_name(self, ServiceNameCatalogMock: MagicMock) -> None:
-        result = get_service_name("name")
-        ServiceNameCatalogMock.find.assert_called_with("name")
-        assert result == ServiceNameCatalogMock.find()
-
-        ServiceNameCatalogMock.find.side_effect = ValueError()
-        assert get_service_name("name") == ServiceNameCatalogMock.create()
-        assert get_service_name("name-none") == ServiceNameCatalogMock.create()
