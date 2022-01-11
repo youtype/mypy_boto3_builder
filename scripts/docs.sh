42adab380dd37b6ea1d72eca93dc25f2d39cfa18
Vlad Emelianov <volshebnyi@gmail.com>
2022-01-11 23:15:51 +0300

    Update docs generation

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -4,4 +4,4 @@ set -e
 ROOT_PATH=$(dirname $(dirname $0))
 cd $ROOT_PATH
 
-handsdown --external `git config --get remote.origin.url` -n mypy-boto3-builder mypy_boto3_builder --exclude '*/build/*' --cleanup --panic
+handsdown --external `git config --get remote.origin.url` -n mypy-boto3-builder mypy_boto3_builder --exclude '*/build/*' --cleanup --panic --branch master
