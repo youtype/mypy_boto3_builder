ba18ca5a6839b2121e0d62d85f758f22c5c165aa
Vlad Emelianov <volshebnyi@gmail.com>
2022-01-29 21:25:25 +0300

    Support list-type references

--- mypy_boto3_builder/parsers/parse_references.py
+++ mypy_boto3_builder/parsers/parse_references.py
@@ -5,6 +5,8 @@ from boto3.resources.base import ServiceResource as Boto3ServiceResource
 
 from mypy_boto3_builder.structures.attribute import Attribute
 from mypy_boto3_builder.type_annotations.internal_import import InternalImport
+from mypy_boto3_builder.type_annotations.type import Type
+from mypy_boto3_builder.type_annotations.type_subscript import TypeSubscript
 
 
 def parse_references(resource: Boto3ServiceResource) -> list[Attribute]:
@@ -22,7 +24,8 @@ def parse_references(resource: Boto3ServiceResource) -> list[Attribute]:
     for reference in references:
         if not reference.resource:
             continue
-        result.append(
-            Attribute(reference.name, type_annotation=InternalImport(reference.resource.type))
-        )
+        type_annotation = InternalImport(reference.resource.type)
+        if reference.resource.path and "[]" in reference.resource.path:
+            type_annotation = TypeSubscript(Type.List, [type_annotation])
+        result.append(Attribute(reference.name, type_annotation=type_annotation))
     return result
