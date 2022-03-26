c3defb55079d73b3bf4f3249717ed640929cc641
Vlad Emelianov <volshebnyi@gmail.com>
2022-03-27 02:09:33 +0300

    Update usage examples and aiobotocore-docs

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -4,4 +4,8 @@ set -e
 ROOT_PATH=$(dirname $(dirname $0))
 cd ${ROOT_PATH}
 
-python -m mypy_boto3_builder ../boto3_stubs_docs/docsmd --product boto3-docs $@
+python -m mypy_boto3_builder ../boto3_stubs_docs_copy/docsmd --product boto3-docs $@
+
+cd ../boto3_stubs_docs_copy/
+mkdocs build
+cd -
