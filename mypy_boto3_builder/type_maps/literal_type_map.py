13f861f8bbeaa38a451e45aef35c508ce0e60ccc
Vlad Emelianov <volshebnyi@gmail.com>
2022-03-28 13:13:38 +0300

    Add BucketLocationConstraintType stub

--- mypy_boto3_builder/type_maps/literal_type_map.py
+++ mypy_boto3_builder/type_maps/literal_type_map.py
@@ -6,10 +6,38 @@ from mypy_boto3_builder.type_annotations.fake_annotation import FakeAnnotation
 from mypy_boto3_builder.type_annotations.type_literal import TypeLiteral
 
 
-LITERAL_TYPE_MAP: dict[ServiceName, dict[str, FakeAnnotation]] = {}
+LITERAL_TYPE_MAP: dict[ServiceName, dict[str, list[str]]] = {
+    ServiceNameCatalog.s3: {
+        # FIXME: https://github.com/boto/botocore/issues/2648
+        "BucketLocationConstraintType": [
+            "af-south-1",
+            "ap-east-1",
+            "ap-northeast-1",
+            "ap-northeast-2",
+            "ap-northeast-3",
+            "ap-south-1",
+            "ap-southeast-1",
+            "ap-southeast-2",
+            "ap-southeast-3",
+            "ca-central-1",
+            "eu-central-1",
+            "eu-north-1",
+            "eu-south-1",
+            "eu-west-1",
+            "eu-west-2",
+            "eu-west-3",
+            "me-south-1",
+            "sa-east-1",
+            "us-east-1",
+            "us-east-2",
+            "us-west-1",
+            "us-west-2",
+        ],
+    },
+}
 
 
-def get_literal_type_stub(service_name: ServiceName, literal_name: str) -> FakeAnnotation | None:
+def get_literal_type_stub(service_name: ServiceName, literal_name: str) -> list[str] | None:
     """
     Get stub type for botocore literal.
 
@@ -18,7 +46,7 @@ def get_literal_type_stub(service_name: ServiceName, literal_name: str) -> FakeA
         literal_name -- Target Literal name.
 
     Returns:
-        Type annotation or None.
+        Literal children or None.
     """
     if service_name not in LITERAL_TYPE_MAP:
         return None
