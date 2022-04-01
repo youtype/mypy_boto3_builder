a090a9a1a99be685f41b4ed468bfe1aed3b890bc
Vlad Emelianov <volshebnyi@gmail.com>
2022-04-01 16:50:36 +0300

    Update proxy publisher

--- scripts/publish_types.sh
+++ scripts/publish_types.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-ROOT_PATH=$(dirname $(dirname $0))
+ROOT_PATH=$(dirname $(dirname $(realpath $0)))
 
 cd $ROOT_PATH/types_boto3
-rm -rf dist/* && python setup.py build sdist bdist_wheel && twine upload dist/*
-
+rm -rf dist/* && python setup.py build sdist bdist_wheel
+twine upload dist/* || true
 
 cd $ROOT_PATH/types_botocore
-rm -rf dist/* && python setup.py build sdist bdist_wheel && twine upload dist/*
+rm -rf dist/* && python setup.py build sdist bdist_wheel
+twine upload dist/* || true
