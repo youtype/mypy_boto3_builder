4582ce237d862c3d9dd5fb543ce2cd079fe14e82
Vlad Emelianov <volshebnyi@gmail.com>
2022-04-27 12:32:23 +0300

    Remove hardcoded BucketLocationConstraintType

--- mypy_boto3_builder/type_maps/literal_type_map.py
+++ mypy_boto3_builder/type_maps/literal_type_map.py
@@ -1,37 +1,9 @@
 """
 String to type annotation map to replace overriden botocore literals.
 """
-from mypy_boto3_builder.service_name import ServiceName, ServiceNameCatalog
+from mypy_boto3_builder.service_name import ServiceName
 
-LITERAL_TYPE_MAP: dict[ServiceName, dict[str, list[str]]] = {
-    ServiceNameCatalog.s3: {
-        # FIXME: https://github.com/boto/botocore/issues/2648
-        "BucketLocationConstraintType": [
-            "af-south-1",
-            "ap-east-1",
-            "ap-northeast-1",
-            "ap-northeast-2",
-            "ap-northeast-3",
-            "ap-south-1",
-            "ap-southeast-1",
-            "ap-southeast-2",
-            "ap-southeast-3",
-            "ca-central-1",
-            "eu-central-1",
-            "eu-north-1",
-            "eu-south-1",
-            "eu-west-1",
-            "eu-west-2",
-            "eu-west-3",
-            "me-south-1",
-            "sa-east-1",
-            "us-east-1",
-            "us-east-2",
-            "us-west-1",
-            "us-west-2",
-        ],
-    },
-}
+LITERAL_TYPE_MAP: dict[ServiceName, dict[str, list[str]]] = {}
 
 
 def get_literal_type_stub(service_name: ServiceName, literal_name: str) -> list[str] | None:
--- tests/type_maps/test_literal_type_map.py
+++ tests/type_maps/test_literal_type_map.py
@@ -3,8 +3,8 @@ from mypy_boto3_builder.type_maps.literal_type_map import get_literal_type_stub
 
 
 def test_get_literal_type_stub() -> None:
-    assert get_literal_type_stub(ServiceNameCatalog.s3, "BucketLocationConstraintType")
-    assert get_literal_type_stub(ServiceNameCatalog.ec2, "BucketLocationConstraintType") is None
+    # assert get_literal_type_stub(ServiceNameCatalog.s3, "BucketLocationConstraintType")
+    assert get_literal_type_stub(ServiceNameCatalog.s3, "BucketLocationConstraintType") is None
     assert (
         get_literal_type_stub(ServiceNameCatalog.cloudformation, "NoneResourceStatusType") is None
     )
