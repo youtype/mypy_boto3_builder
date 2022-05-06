5ef07742b819eb3a59d832f5551bc139a372583f
Vlad Emelianov <volshebnyi@gmail.com>
2022-05-06 17:22:13 +0300

    Use TypedDict in dynamodb.TypeDeserializer

--- mypy_boto3_builder/boto3_stubs_static/dynamodb/types.pyi
+++ mypy_boto3_builder/boto3_stubs_static/dynamodb/types.pyi
@@ -1,13 +1,30 @@
 import sys
 from decimal import Context
-from typing import Any, Dict, Mapping, Tuple
+from typing import Any, Mapping, Tuple, Sequence
 
 if sys.version_info >= (3, 9):
-    from typing import Literal
+    from typing import Literal, TypedDict
 else:
-    from typing_extensions import Literal
+    from typing_extensions import Literal, TypedDict
 
 DynamoDBDataType = Literal["S", "N", "B", "SS", "NS", "BS", "NULL", "BOOL", "M", "L"]
+
+_AttributeValueTypeDef = TypedDict(
+    "_AttributeValueTypeDef",
+    {
+        "S": str,
+        "N": str,
+        "B": bytes,
+        "SS": Sequence[str],
+        "NS": Sequence[str],
+        "BS": Sequence[bytes],
+        "M": Mapping[str, Any],
+        "L": Sequence[Any],
+        "NULL": bool,
+        "BOOL": bool,
+    },
+    total=False,
+)
 
 STRING: Literal["S"]
 NUMBER: Literal["N"]
@@ -33,7 +50,7 @@ class Binary:
     def __hash__(self) -> int: ...
 
 class TypeSerializer:
-    def serialize(self, value: Any) -> Dict[DynamoDBDataType, Any]: ...
+    def serialize(self, value: Any) -> _AttributeValueTypeDef: ...
 
 class TypeDeserializer:
-    def deserialize(self, value: Mapping[DynamoDBDataType, Any]) -> Any: ...
+    def deserialize(self, value: _AttributeValueTypeDef) -> Any: ...
