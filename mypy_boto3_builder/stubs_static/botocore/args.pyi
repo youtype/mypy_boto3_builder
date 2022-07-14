9569548640410668ff386ab9014ad99e9169563c
Vlad Emelianov <volshebnyi@gmail.com>
2022-07-15 02:01:36 +0300

    Fix inconsistency in types-aiobotocore ClientArgsCreator

--- mypy_boto3_builder/stubs_static/botocore/args.pyi
+++ mypy_boto3_builder/stubs_static/botocore/args.pyi
@@ -1,16 +1,36 @@
+import sys
 from typing import Any, Dict, List, Optional, Union
 
 from botocore.config import Config as Config
 from botocore.configprovider import ConfigValueStore
+from botocore.endpoint import Endpoint
 from botocore.endpoint import EndpointCreator as EndpointCreator
 from botocore.hooks import BaseEventHooks
 from botocore.loaders import Loader
 from botocore.model import ServiceModel
-from botocore.parsers import ResponseParserFactory
+from botocore.parsers import ResponseParser, ResponseParserFactory
+from botocore.serialize import BaseRestSerializer
 from botocore.signers import RequestSigner as RequestSigner
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
 
 VALID_REGIONAL_ENDPOINTS_CONFIG: List[str]
 LEGACY_GLOBAL_STS_REGIONS: List[str]
+
+class GetClientArgsTypeDef(TypedDict):
+    serializer: BaseRestSerializer
+    endpoint: Endpoint
+    response_parser: ResponseParser
+    event_emitter: Any
+    request_signer: RequestSigner
+    service_model: ServiceModel
+    loader: Loader
+    client_config: Config
+    partition: Optional[str]
+    exceptions_factory: Any
 
 class ClientArgsCreator:
     def __init__(
@@ -24,7 +44,7 @@ class ClientArgsCreator:
     ) -> None: ...
     def get_client_args(
         self,
-        service_model: str,
+        service_model: ServiceModel,
         region_name: str,
         is_secure: bool,
         endpoint_url: Optional[str],
@@ -33,7 +53,7 @@ class ClientArgsCreator:
         scoped_config: Optional[Any],
         client_config: Optional[Config],
         endpoint_bridge: Optional[Any],
-    ) -> Any: ...
+    ) -> GetClientArgsTypeDef: ...
     def compute_client_args(
         self,
         service_model: ServiceModel,
