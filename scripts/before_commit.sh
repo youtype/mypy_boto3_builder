599967c0dbb2721fd3e7c7b6fb2a2c9c76402e6e
Vlad Emelianov <volshebnyi@gmail.com>
2022-07-21 20:20:25 +0300

    Use poetry in before_commit

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -4,14 +4,14 @@ set -e
 ROOT_PATH=$(dirname $(dirname $0))
 cd $ROOT_PATH
 
-# vulture mypy_boto3_builder --make-whitelist > vulture_whitelist.txt
-vulture mypy_boto3_builder vulture_whitelist.txt
+# poetry run vulture mypy_boto3_builder --make-whitelist > vulture_whitelist.txt
+poetry run vulture mypy_boto3_builder vulture_whitelist.txt
 npx pyright mypy_boto3_builder
-flake8 mypy_boto3_builder
-pytest
-black mypy_boto3_builder tests
-isort mypy_boto3_builder tests
-mypy mypy_boto3_builder
-# pytest --cov-report html --cov mypy_boto3_builder
+poetry run flake8 mypy_boto3_builder
+poetry run pytest
+poetry run black mypy_boto3_builder tests
+poetry run isort mypy_boto3_builder tests
+poetry run mypy mypy_boto3_builder
+# poetry run pytest --cov-report html --cov mypy_boto3_builder
 
 ./scripts/docs.sh
