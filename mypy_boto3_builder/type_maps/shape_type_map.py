952c4c1b954071f687fb82c7179b05c10328289e
Vlad Emelianov <volshebnyi@gmail.com>
2022-12-22 11:03:58 +0300

    Update deps, lint

--- mypy_boto3_builder/type_maps/shape_type_map.py
+++ mypy_boto3_builder/type_maps/shape_type_map.py
@@ -8,7 +8,6 @@ from mypy_boto3_builder.import_helpers.import_string import ImportString
 from mypy_boto3_builder.service_name import ServiceName, ServiceNameCatalog
 from mypy_boto3_builder.type_annotations.external_import import ExternalImport
 from mypy_boto3_builder.type_annotations.fake_annotation import FakeAnnotation
-from mypy_boto3_builder.type_annotations.internal_import import InternalImport
 from mypy_boto3_builder.type_annotations.type import Type
 from mypy_boto3_builder.type_annotations.type_subscript import TypeSubscript
 from mypy_boto3_builder.type_annotations.type_typed_dict import TypedDictAttribute, TypeTypedDict
