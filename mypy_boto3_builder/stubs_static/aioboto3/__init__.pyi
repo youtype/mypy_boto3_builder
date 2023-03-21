f0b139f38f465f1a9579740cd77645eb64541585
Vlad Emelianov <volshebnyi@gmail.com>
2023-03-21 23:58:03 +0300

    Add missing annotations to aioboto3

--- mypy_boto3_builder/stubs_static/aioboto3/__init__.pyi
+++ mypy_boto3_builder/stubs_static/aioboto3/__init__.pyi
@@ -3,7 +3,9 @@ from typing import Any
 
 from aioboto3.session import Session as Session
 
+__author__: str
 __email__: str
+__version__: str
 
 class NullHandler(logging.Handler):
     def emit(self, record: Any) -> Any: ...
