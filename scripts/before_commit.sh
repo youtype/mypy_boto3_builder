cbe8ffa86f090af4f0634ddbb7b553eeaf1c28b3
Vlad Emelianov <volshebnyi@gmail.com>
2023-11-09 00:27:20 +0300

    Reformat

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -6,11 +6,11 @@ cd $ROOT_PATH
 
 # poetry run vulture mypy_boto3_builder --make-whitelist > vulture_whitelist.txt
 poetry run vulture mypy_boto3_builder vulture_whitelist.txt
-poetry run npx pyright mypy_boto3_builder
+poetry run npx pyright .
 poetry run flake8 mypy_boto3_builder
 poetry run pytest
-poetry run black mypy_boto3_builder tests
-poetry run isort mypy_boto3_builder tests
+poetry run black .
+poetry run isort .
 poetry run mypy mypy_boto3_builder
 # poetry run pytest --cov-report html --cov mypy_boto3_builder
 
