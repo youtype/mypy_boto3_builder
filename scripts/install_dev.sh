24dbb6a46198cbcc225188e1259b4a63f82fe7a2
Vlad Emelianov <volshebnyi@gmail.com>
2023-12-13 22:37:55 +0300

    Do not use install dev in CI, add mkdocs to dev install

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+ROOT_PATH=$(dirname $(dirname $0))
+
 poetry install -n
 poetry run pip install -U aioboto3 boto3-stubs types-aiobotocore types-aioboto3
+poetry run pip install -r ${ROOT_PATH}/requirements.mkdocs.txt
 poetry install -n
