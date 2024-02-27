75c044742f7d841f85de0adeb8b0438dde532ef8
Vlad Emelianov <volshebnyi@gmail.com>
2024-02-27 15:34:57 +0300

    Add stub for PlatformValuesType

--- mypy_boto3_builder/type_maps/literal_type_map.py
+++ mypy_boto3_builder/type_maps/literal_type_map.py
@@ -2,9 +2,12 @@
 String to type annotation map to replace overriden botocore literals.
 """
 
-from mypy_boto3_builder.service_name import ServiceName
+from mypy_boto3_builder.service_name import ServiceName, ServiceNameCatalog
 
-LITERAL_TYPE_MAP: dict[ServiceName, dict[str, list[str]]] = {}
+LITERAL_TYPE_MAP: dict[ServiceName, dict[str, list[str]]] = {
+    # FIXME: https://github.com/boto/botocore/issues/3128
+    ServiceNameCatalog.ec2: {"PlatformValuesType": ["windows"]}
+}
 
 
 def get_literal_type_stub(service_name: ServiceName, literal_name: str) -> list[str] | None:
