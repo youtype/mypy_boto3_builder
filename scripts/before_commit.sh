42585a53adcb7618da5d2373ce02a9fc13ba301b
Vlad Emelianov <volshebnyi@gmail.com>
2024-09-18 21:30:43 +0300

    Use uv tools

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -4,9 +4,9 @@ set -e
 ROOT_PATH=$(dirname $(dirname $0))
 cd $ROOT_PATH
 
-# uv run vulture mypy_boto3_builder --make-whitelist > vulture_whitelist.txt
-uv run pre-commit run --all-files
+# uvx vulture mypy_boto3_builder --make-whitelist > vulture_whitelist.txt
+uvx pre-commit run --all-files
 
-# uv run pytest --cov-report html --cov mypy_boto3_builder
+# uvx pytest --cov-report html --cov mypy_boto3_builder
 
 # ./scripts/docs.sh
