c6a7b8a5b8c3c6945e066004722b3bdaddc81343
Vlad Emelianov <vlad.emelianov.nz@gmail.com>
2024-12-02 01:44:03 +0300

    Add chat-like interface for ChatBuddy

--- mypy_boto3_builder/chat_buddy.py
+++ mypy_boto3_builder/chat_buddy.py
@@ -6,11 +6,12 @@ Copyright 2024 Vlad Emelianov
 
 import logging
 import sys
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
 from pathlib import Path
 
 import questionary
 from colorama import Fore, Style, just_fix_windows_console
+from prompt_toolkit.shortcuts.prompt import CompleteStyle
 from prompt_toolkit.validation import ValidationError
 
 from mypy_boto3_builder.cli_parser import CLINamespace
@@ -22,10 +23,14 @@ from mypy_boto3_builder.service_name import ServiceName
 from mypy_boto3_builder.utils.boto3_utils import get_available_service_names
 from mypy_boto3_builder.utils.path import print_path
 
-MAX_SERVICE_PRINTED = 10
-MAX_SERVICE_SELECTABLE = 10
+MAX_SERVICE_PRINTED = 20
+MAX_SERVICE_SELECTABLE = 20
 DEFAULT_OUTPUT_PATH = Path("./vendored")
 REPORT_URL = "https://github.com/youtype/mypy_boto3_builder/issues"
+QMARK = "Input:  "
+BUILDER_PREFIX = "Builder:"
+YOU_PREFIX = "You:    "
+PAIR = 2
 
 
 class ServiceActions:
@@ -33,11 +38,11 @@ class ServiceActions:
     Available service actions.
     """
 
-    build = "Let's build!"
-    add = "Select another service"
-    add_all = "Select all available services"
-    remove = "Remove selected service"
-    remove_all = "Remove all selected services"
+    build = "continue"
+    add = "add another service"
+    add_all = "add all available services"
+    remove = "remove selected service"
+    remove_all = "remove all selected services"
 
 
 class PackageManagerChoices:
@@ -49,15 +54,25 @@ class PackageManagerChoices:
     poetry = "poetry"
     pipenv = "pipenv"
     uv = "uv"
-    unknown = "I dont' know :("
+    unknown = "... I dont' know :("
 
 
-def _tag(message: str | float) -> str:
-    return f"{Fore.CYAN}{Style.BRIGHT}{message}{Style.RESET_ALL}"
+def _tag(message: str | float, color: str = Fore.CYAN) -> str:
+    return f"{color}{Style.BRIGHT}{message}{Style.RESET_ALL}"
 
 
-def _quote(message: str, name: str = "Builder") -> str:
-    return f"{Fore.MAGENTA}{Style.BRIGHT}{name}:{Style.RESET_ALL}{Fore.RESET} {message}"
+def _linebreak() -> None:
+    sys.stdout.write("\n")
+
+
+def _join_and(items: Sequence[str]) -> str:
+    if not items:
+        return ""
+    if len(items) == 1:
+        return f"only {items[0]}"
+    if len(items) == PAIR:
+        return f"{items[0]} and {items[1]}"
+    return f"{', '.join(items[:-1])}, and {items[-1]}"
 
 
 class ChatBuddy:
@@ -68,6 +83,7 @@ class ChatBuddy:
     def __init__(self) -> None:
         self.available_service_names: tuple[ServiceName, ...] = ()
         self.selected_service_names: list[ServiceName] = []
+        self.initial_service_names: list[ServiceName] = []
         self.product_library = ProductLibrary.boto3
         self.library_name: str = ""
         self.product: Product = Product.types_boto3_custom
@@ -89,13 +105,18 @@ class ChatBuddy:
 
     @staticmethod
     def _say(message: str) -> None:
-        sys.stdout.write(f"{_quote(message)}\n")
+        sys.stdout.write(f"{_tag(BUILDER_PREFIX, Fore.MAGENTA)} {message}\n\n")
+
+    def _respond(self, message: str) -> None:
+        sys.stdout.write(f"{_tag(YOU_PREFIX, Fore.GREEN)} {message}\n\n")
 
     def _select_service_name(
-        self, title_suffix: str, service_names: list[ServiceName]
+        self, message: str, service_names: list[ServiceName]
     ) -> ServiceName | None:
-        choices = {i.class_name: i for i in service_names}
+        choices = {f"{i.class_name} ({i.boto3_name})": i for i in service_names}
         lower_choices = {k.lower(): v for k, v in choices.items()}
+        lower_choices.update({i.class_name.lower(): i for i in service_names})
+        lower_choices.update({i.boto3_name.lower(): i for i in service_names})
         selected_choice: str
         if len(choices) > MAX_SERVICE_SELECTABLE:
 
@@ -104,16 +125,23 @@ class ChatBuddy:
                     raise ValidationError(len(choice), f"{choice} service does not exist")
                 return True
 
+            self._say("Start typing service name so I can find it in the list.")
             selected_choice = questionary.autocomplete(
-                f"Enter service name {title_suffix}",
+                message=message,
+                qmark=QMARK,
                 choices=list(choices),
+                complete_style=CompleteStyle.COLUMN,
                 validate=_validate,
             ).unsafe_ask()
+            _linebreak()
         else:
+            self._say("Which one?")
             selected_choice = questionary.select(
-                f"Select service {title_suffix}",
-                choices=["<< Go back", *choices],
+                message=message,
+                qmark=QMARK,
+                choices=["nothing, go back", *choices],
             ).unsafe_ask()
+            _linebreak()
         if not selected_choice:
             return None
         key = selected_choice.lower()
@@ -125,34 +153,43 @@ class ChatBuddy:
     def _get_services_message(self) -> str:
         selected = self.selected_service_names
         if self._is_all_selected():
-            selected_str = f"{_tag('ALL')} are selected"
-        elif not selected:
-            selected_str = f"{_tag('NONE')} selected"
-        else:
-            selected_str = ", ".join(
-                _tag(service_name.class_name)
-                if index < MAX_SERVICE_PRINTED
-                else f"and {_tag(len(selected) - MAX_SERVICE_PRINTED)} other"
-                for (index, service_name) in enumerate(selected[: MAX_SERVICE_PRINTED + 1])
+            return (
+                f"Do you want type annotations for {_tag('ALL')} available services?"
+                f" It will take {_tag('10-20 minutes')}!"
             )
-            selected_str = f"{selected_str} {'are' if len(selected) > 1 else 'is'} selected"
-        return (
-            f"From {_tag(len(self.available_service_names))} available"
-            f" {_tag(self.product_library.value)} services {selected_str}."
-        )
+        if not selected:
+            return f"Okay, what service do you want to {_tag('add')}?"
 
-    def _select_library(self) -> ProductLibrary:
+        if len(selected) == 1:
+            return (
+                f"Should we build type annotations only for {_tag(selected[0].class_name)} service?"
+            )
+
+        if len(selected) > MAX_SERVICE_PRINTED:
+            selected_strs = [
+                *(_tag(i.class_name) for i in selected[: MAX_SERVICE_PRINTED - 1]),
+                f"{_tag(len(selected) - MAX_SERVICE_PRINTED + 1)} other",
+            ]
+        else:
+            selected_strs = [_tag(i.class_name) for i in selected]
+        selected_str = _join_and(selected_strs)
+        return f"Do you use {selected_str} services?"
+
+    def _select_library(self) -> ProductLibrary | None:
         library_choices = {
             "boto3": ProductLibrary.boto3,
             "aiobotocore": ProductLibrary.aiobotocore,
             "aioboto3": ProductLibrary.aioboto3,
             "boto3 (legacy boto3-stubs)": ProductLibrary.boto3_legacy,
+            "none of them": None,
         }
         selected = questionary.select(
-            "",
+            "My project uses",
+            qmark=QMARK,
             choices=list(library_choices),
             default=ProductLibrary.boto3.value,
         ).unsafe_ask()
+        _linebreak()
         return library_choices[selected]
 
     def _select_product(self) -> Product:
@@ -181,10 +218,12 @@ class ChatBuddy:
                 )
             )
             response = questionary.select(
-                message="What do you want to do?",
+                "Let's",
+                qmark=QMARK,
                 choices=response_choices,
                 default=response_choices[0],
             ).unsafe_ask()
+            _linebreak()
             match response:
                 case ServiceActions.build:
                     return result
@@ -193,7 +232,8 @@ class ChatBuddy:
                     choice_service_names = [
                         i for i in self.available_service_names if i not in result_set
                     ]
-                    service_name = self._select_service_name("to add", choice_service_names)
+                    message = "I use" if not result else "I also use"
+                    service_name = self._select_service_name(message, choice_service_names)
                     if service_name is None:
                         continue
                     result.append(service_name)
@@ -203,7 +243,7 @@ class ChatBuddy:
                     result.extend(self.available_service_names)
                     continue
                 case ServiceActions.remove:
-                    service_name = self._select_service_name("to remove", result)
+                    service_name = self._select_service_name("I do not use", result)
                     if service_name is None:
                         continue
                     result.remove(service_name)
@@ -260,7 +300,8 @@ class ChatBuddy:
 
     def _select_package_manager(self) -> str:
         result = questionary.select(
-            "",
+            "My package manager is",
+            qmark=QMARK,
             choices=[
                 PackageManagerChoices.uv,
                 PackageManagerChoices.pip,
@@ -270,7 +311,9 @@ class ChatBuddy:
             ],
             default=PackageManagerChoices.uv,
         ).unsafe_ask()
+        _linebreak()
         if result == PackageManagerChoices.unknown:
+            self._say("Then let's use pip to install the package.")
             return PackageManagerChoices.pip
 
         return result
@@ -281,13 +324,15 @@ class ChatBuddy:
                 raise ValidationError(0, "Path should not be empty")
             return True
 
-        return Path(
-            questionary.path(
-                "Path to output directory",
-                default=DEFAULT_OUTPUT_PATH.as_posix(),
-                validate=_validate,
-            ).unsafe_ask()
-        )
+        response = questionary.path(
+            message="Save package in",
+            qmark=QMARK,
+            default=DEFAULT_OUTPUT_PATH.as_posix(),
+            complete_style=CompleteStyle.READLINE_LIKE,
+            validate=_validate,
+        ).unsafe_ask()
+        _linebreak()
+        return Path(response)
 
     def _get_cli_namespace(self) -> CLINamespace:
         return CLINamespace(
@@ -302,61 +347,107 @@ class ChatBuddy:
         )
 
     def _do_start_building(self) -> bool:
-        return questionary.confirm(f"Start building {self.product.value}?").unsafe_ask()
+        response = questionary.confirm(
+            message=f"Start building {self.product.value}?",
+            qmark=QMARK,
+        ).unsafe_ask()
+        _linebreak()
+        return response
 
-    def run(self, run_builder: Callable[[CLINamespace], None]) -> None:
+    def _get_response_services(self) -> str:
+        if self._is_all_selected():
+            return "Let's include all the services. Just in case. I might need them later."
+
+        if self.selected_service_names == self.initial_service_names:
+            return "Looks good. I use the most popular services."
+
+        services_str = _join_and([i.class_name for i in self.selected_service_names])
+        return f"Yes, I use {services_str} services."
+
+    def run(self, run_builder: Callable[[CLINamespace], None]) -> None:  # noqa: PLR0915
         """
         Run chat buddy.
         """
         just_fix_windows_console()
-        self._say(f"Hello and welcome to {_tag(PROG_NAME)}!")
+        self._say(f"Hello from {_tag(PROG_NAME)}!")
         self._say(
-            f"It looks like you want to add {_tag('type annotations')} to your project,"
-            " but you launched me with no command-line arguments. So I decided to help you a bit!"
+            f"It looks like you plan to add {_tag('boto3')}, {_tag('aioboto3')},"
+            f" or {_tag('aiobotocore')} type annotations to your project."
+        )
+        self._say(
+            f"You launched me with no {_tag('OUTPUT_PATH')} command-line argument,"
+            f" so I decided to help you a bit."
         )
         self._say(f"Remember, if anything goes wrong, report to {_tag(REPORT_URL)}")
         self._say(f"First of all, what {_tag('AWS SDK library')} do you use?")
-        self.product_library = self._select_library()
+        product_library = self._select_library()
+        if not product_library:
+            self._respond("No, I do not use any of these libraries.")
+            self._say("No worries, you can always run me again if you start using them.")
+            self._finish()
+            return
+
+        self.product_library = product_library
+        self.library_name = self.product_library.get_library_name()
+        self._respond(
+            f"I use {_tag(self.library_name)} in this project."
+            f" Now, how to build {_tag('type annotations')} for it?"
+        )
 
         self.product = self._select_product()
 
-        self.library_name = self.product_library.get_library_name()
         self._say(
-            "Good choice! Hold on, I am fetching all available services for"
-            f" {_tag(self.library_name)}..."
+            f"Just a sec! I am fetching all available services for {_tag(self.library_name)}..."
         )
         self.available_service_names = tuple(get_available_service_names())
-        self.selected_service_names = self._get_selected_service_names()
+        self.initial_service_names = self._get_selected_service_names()
+        self.selected_service_names = list(self.initial_service_names)
+        self._say("Thanks for waiting!")
         self._say(
-            f"Found {_tag(len(self.available_service_names))} services"
-            f" for {_tag(self.library_name)}. However, most projects use only"
+            f"There are {_tag(len(self.available_service_names))} supported services"
+            f" for {_tag(self.library_name)}."
+            " However, most projects use only"
             f" {_tag(len(self.selected_service_names))} of them."
-            " Let me know if you use any other services or want to remove some."
+            " Let me know if you use any other services."
         )
         self.selected_service_names = self._select_services()
+        self._respond(self._get_response_services())
+
+        if len(self.selected_service_names) == 1:
+            services_str = f"{_tag(self.selected_service_names[0].class_name)} service support"
+        else:
+            services_str = f"{_tag(len(self.selected_service_names))} services support"
 
         self._say(
             f"Great! Now I am ready to generate type annotations for {_tag(self.library_name)}"
-            f" with {_tag(len(self.selected_service_names))} included services."
+            f" with {services_str}."
         )
         self._say(
-            "One more thing."
-            f" Where should I put a generated {_tag(self.product.value)} package?"
-            f" I prefer to use {_tag(print_path(self.output_path))} directory,"
-            " but you can change it."
+            f"Almost forgot... Where should I put a generated {_tag(self.product.value)} package?"
+        )
+        self._say(
+            f"I prefer to use {_tag(print_path(self.output_path))} directory,"
+            " but it is up to you."
         )
         self.output_path = self._select_output_path()
+        self._respond(
+            f"Save the package in {_tag(print_path(self.output_path))}."
+            f" I might even add it to {_tag('git')}!"
+        )
 
         self.args = self._get_cli_namespace()
 
         self._say(f"Got it! I can start building {_tag(self.product.value)} now if you want.")
         if not self._do_start_building():
+            self._respond("No, I just want to check how to do it.")
             self._say(f"No worries, you can always build {_tag(self.product.value)} later!")
             self._say_commands()
             self._finish()
             return
 
+        self._respond("Go for it!")
         run_builder(self.args)
+        _linebreak()
 
         found_whl = self._find_last_whl()
         if not found_whl:
@@ -368,17 +459,20 @@ class ChatBuddy:
             self._finish()
             return
 
-        self._say(f"All done! I have built {_tag(print_path(found_whl))}. Let's install it!")
+        self._say(f"I have built {_tag(print_path(found_whl))}, and it is ready to install.")
 
         self._say(
             f"Let me know what {_tag('package manager')} you use."
-            f" I can recommend {_tag('uv')}, because it is the best."
+            f" I can recommend {_tag('uv')}, because it is extremely fast."
             " But you can choose any other."
         )
         self.package_manager = self._select_package_manager()
+        self._respond(
+            f"Sure, I use {_tag(self.package_manager)}. How to install the generated package?"
+        )
 
         self._say(
-            f"All done! Use this commands to {_tag('update')}"
+            f"Use these commands to {_tag('update')}"
             f" and {_tag('install')} {_tag(self.product.value)}"
             f" when you bump {_tag(self.library_name)} version:"
         )
