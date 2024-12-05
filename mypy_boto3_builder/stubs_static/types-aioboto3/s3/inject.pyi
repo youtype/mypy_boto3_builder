d64785dcbbfc9a809ad13dc7f0d46ebd06c283d8
Vlad Emelianov <vlad.emelianov.nz@gmail.com>
2024-12-05 21:38:07 +0300

    Sync static stubs

--- mypy_boto3_builder/stubs_static/types-aioboto3/s3/inject.pyi
+++ mypy_boto3_builder/stubs_static/types-aioboto3/s3/inject.pyi
@@ -5,18 +5,28 @@ Copyright 2024 Vlad Emelianov
 """
 
 import logging
+import sys
 from abc import abstractmethod
 from typing import Any, BinaryIO, Callable, Sequence
 
 from boto3.s3.transfer import TransferConfig as S3TransferConfig
 
+if sys.version_info >= (3, 12):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
 logger: logging.Logger
+
+TransferCallback: TypeAlias = Callable[[int], None]
 
 class _AsyncBinaryIO:
     @abstractmethod
     async def seek(self, offset: int, whence: int = 0) -> int: ...
     @abstractmethod
     async def write(self, s: bytes | bytearray) -> int: ...
+
+AnyFileObject: TypeAlias = _AsyncBinaryIO | BinaryIO
 
 def inject_s3_transfer_methods(class_attributes: Sequence[Any], **kwargs: Any) -> None: ...
 def inject_object_summary_methods(class_attributes: Sequence[Any], **kwargs: Any) -> None: ...
@@ -28,25 +38,25 @@ async def download_file(
     Key: str,
     Filename: str,
     ExtraArgs: list[str] | None = ...,
-    Callback: Callable[[int], None] | None = ...,
+    Callback: TransferCallback | None = ...,
     Config: S3TransferConfig | None = ...,
 ) -> None: ...
 async def download_fileobj(
     self: Any,
     Bucket: str,
     Key: str,
-    Fileobj: _AsyncBinaryIO | BinaryIO,
+    Fileobj: AnyFileObject,
     ExtraArgs: dict[str, Any] | None = ...,
-    Callback: Callable[[int], None] | None = ...,
+    Callback: TransferCallback | None = ...,
     Config: S3TransferConfig | None = ...,
 ) -> None: ...
 async def upload_fileobj(
     self: Any,
-    Fileobj: _AsyncBinaryIO | BinaryIO,
+    Fileobj: AnyFileObject,
     Bucket: str,
     Key: str,
     ExtraArgs: dict[str, Any] | None = ...,
-    Callback: Callable[[int], None] | None = ...,
+    Callback: TransferCallback | None = ...,
     Config: S3TransferConfig | None = ...,
     Processing: Callable[[bytes], bytes] | None = ...,
 ) -> Any: ...
@@ -56,7 +66,7 @@ async def upload_file(
     Bucket: str,
     Key: str,
     ExtraArgs: dict[str, Any] | None = ...,
-    Callback: Callable[[int], None] | None = ...,
+    Callback: TransferCallback | None = ...,
     Config: S3TransferConfig | None = ...,
 ) -> None: ...
 async def copy(
@@ -65,7 +75,7 @@ async def copy(
     Bucket: str,
     Key: str,
     ExtraArgs: dict[str, Any] | None = ...,
-    Callback: Callable[[int], None] | None = ...,
+    Callback: TransferCallback | None = ...,
     SourceClient: Any | None = ...,
     Config: S3TransferConfig | None = ...,
 ) -> None: ...
--- mypy_boto3_builder/stubs_static/types-aioboto3/session.pyi
+++ mypy_boto3_builder/stubs_static/types-aioboto3/session.pyi
@@ -12,11 +12,12 @@ from aioboto3.resources.factory import AIOBoto3ResourceFactory
 from aiobotocore.client import AioBaseClient
 from aiobotocore.config import AioConfig
 from aiobotocore.credentials import AioCredentials
-from botocore.hooks import BaseEventHooks
+from aiobotocore.session import ClientCreatorContext
+from boto3.session import Session as Boto3Session
 from botocore.loaders import Loader
 from botocore.session import Session as BotocoreSession
 
-class Session:
+class Session(Boto3Session):
     def __init__(
         self,
         aws_access_key_id: str | None = ...,
@@ -30,26 +31,7 @@ class Session:
         self.resource_factory: AIOBoto3ResourceFactory
         self._loader: Loader
 
-    @property
-    def profile_name(self) -> str: ...
-    @property
-    def region_name(self) -> str: ...
-    @property
-    def events(self) -> BaseEventHooks: ...
-    @property
-    def available_profiles(self) -> list[str]: ...
-    def _setup_loader(self) -> None: ...
-    def get_available_services(self) -> list[str]: ...
-    def get_available_resources(self) -> list[str]: ...
-    def get_available_partitions(self) -> list[str]: ...
-    def get_available_regions(
-        self,
-        service_name: str,
-        partition_name: str = ...,
-        allow_non_regional: bool = ...,
-    ) -> list[str]: ...
     def get_credentials(self) -> AioCredentials | None: ...
-    def _register_default_handlers(self) -> None: ...
     def client(  # type: ignore [override]
         self,
         service_name: str,
@@ -62,8 +44,8 @@ class Session:
         aws_secret_access_key: str | None = ...,
         aws_session_token: str | None = ...,
         config: AioConfig | None = ...,
-    ) -> AioBaseClient: ...
-    def resource(
+    ) -> ClientCreatorContext[AioBaseClient]: ...
+    def resource(  # type: ignore [override]
         self,
         service_name: str,
         region_name: str | None = ...,
