a9cece3ce50c39650384ad07290024caba1cc845
Vlad Emelianov <vlad.emelianov.nz@gmail.com>
2024-12-06 18:45:16 +0300

    Pull static

--- mypy_boto3_builder/stubs_static/types-boto3/__init__.pyi
+++ mypy_boto3_builder/stubs_static/types-boto3/__init__.pyi
@@ -5,17 +5,18 @@ Copyright 2024 Vlad Emelianov
 """
 
 import logging
-from typing import Any
 
 from boto3 import session as session
+from boto3.resources.base import ServiceResource
 from boto3.session import Session as Session
+from botocore.client import BaseClient
 from botocore.config import Config
 from botocore.session import Session as BotocoreSession
 
-__author__: str
-__version__: str
+__author__: str = ...
+__version__: str = ...
 
-DEFAULT_SESSION: Session | None
+DEFAULT_SESSION: Session | None = ...
 
 def setup_default_session(
     *,
@@ -27,12 +28,13 @@ def setup_default_session(
     profile_name: str | None = ...,
 ) -> None: ...
 def set_stream_logger(
-    name: str = ..., level: int = ..., format_string: str | None = ...
+    name: str = ...,
+    level: int = ...,
+    format_string: str | None = ...,
 ) -> None: ...
 def _get_default_session() -> Session: ...
 
-class NullHandler(logging.Handler):
-    def emit(self, record: Any) -> Any: ...
+class NullHandler(logging.Handler): ...
 
 def client(
     service_name: str,
@@ -45,7 +47,7 @@ def client(
     aws_secret_access_key: str | None = ...,
     aws_session_token: str | None = ...,
     config: Config | None = ...,
-) -> Any: ...
+) -> BaseClient: ...
 def resource(
     service_name: str,
     region_name: str | None = ...,
@@ -57,4 +59,4 @@ def resource(
     aws_secret_access_key: str | None = ...,
     aws_session_token: str | None = ...,
     config: Config | None = ...,
-) -> Any: ...
+) -> ServiceResource: ...
