7838220c93af798410d8ff43bd192af3047185c1
Vlad Emelianov <vlad.emelianov.nz@gmail.com>
2024-12-07 04:50:02 +0300

    Revert --no-project

--- scripts/build_main.sh
+++ scripts/build_main.sh
@@ -2,13 +2,13 @@
 set -e
 
 # types-boto3
-uv run --no-project --with ruff --with boto3 mypy_boto3_builder mypy_boto3_output --product types-boto3 types-boto3-lite
+uv run --with ruff --with boto3 mypy_boto3_builder mypy_boto3_output --product types-boto3 types-boto3-lite
 
 # types-aiobotocore
-uv run --no-project --with ruff --with aiobotocore mypy_boto3_builder mypy_boto3_output --product aiobotocore aiobotocore-lite
+uv run --with ruff --with aiobotocore mypy_boto3_builder mypy_boto3_output --product aiobotocore aiobotocore-lite
 
 # types-aioboto3
-uv run --no-project --with ruff --with aioboto3 mypy_boto3_builder mypy_boto3_output --product aioboto3 aioboto3-lite
+uv run --with ruff --with aioboto3 mypy_boto3_builder mypy_boto3_output --product aioboto3 aioboto3-lite
 
 # boto3-stubs
-uv run --no-project --with ruff --with boto3 mypy_boto3_builder mypy_boto3_output --product boto3 boto3-lite
+uv run --with ruff --with boto3 mypy_boto3_builder mypy_boto3_output --product boto3 boto3-lite
