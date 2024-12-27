97d0994e9b305d04c1ee9fa50dbf2d7d89f62e7e
Vlad Emelianov <vlad.emelianov.nz@gmail.com>
2024-12-28 02:51:06 +0300

    Fix aioboto3 package version requirements

--- mypy_boto3_builder/structures/packages/types_aioboto3_package.py
+++ mypy_boto3_builder/structures/packages/types_aioboto3_package.py
@@ -5,6 +5,8 @@ Copyright 2024 Vlad Emelianov
 """
 
 from mypy_boto3_builder.structures.packages.wrapper_package import WrapperPackage
+from mypy_boto3_builder.utils.version import get_max_build_version, get_min_build_version
+from mypy_boto3_builder.utils.version_getters import get_aiobotocore_version
 
 
 class TypesAioBoto3Package(WrapperPackage):
@@ -20,3 +22,17 @@ class TypesAioBoto3Package(WrapperPackage):
             "Session",
         ]
         return sorted(result)
+
+    @property
+    def min_library_version(self) -> str:
+        """
+        Minimum required library version.
+        """
+        return get_min_build_version(get_aiobotocore_version())
+
+    @property
+    def max_library_version(self) -> str:
+        """
+        Minimum required library version.
+        """
+        return get_max_build_version(get_aiobotocore_version())
